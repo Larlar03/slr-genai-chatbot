a0dd2fbd924a10896e38825dfac39dcb0388fb61
Lana Work <37781167+Larlar03@users.noreply.github.com>
2023-07-19 14:45:00 +0100

    broke script down into functions

--- index.py
+++ index.py
@@ -7,31 +7,46 @@ URL = "https://www.artrabbit.com/all-listings/united-kingdom/birmingham?page={pa
 
 page = requests.get(URL)
 
-for num in range(1, 2):
-    page = requests.get(URL.format(page_number=str(num)))
-    soup = BeautifulSoup(page.content, "html.parser")
+
+def getEventDetails(article):
+    category = article.find(class_="b_categorical-heading").text
+    # print(category.text)
+
+    title = article.find_all(class_="b_small-heading")[0].text
+    date = article.find_all(class_="b_small-heading")[1].text
+    # print(title.text, date.text)
+
+    venue = article.find_all(class_="b_instructional-text")[0].text
+    location = article.find_all(class_="b_instructional-text")[1].text
+
+    return category, title, date, venue, location
+    # print(venue.text, location.text)
+
+
+def getEventLink(article):
+    link = article.find("a").get("href")
+    return link
+
+
+def getEventImage(article):
+    image = article.find("picture").find("img")
+    image_src = image.get("src")
+    image_alt = image.get("alt")
+    return image_alt, image_src
+
+
+for page in range(1, 3):
+    r = requests.get(URL.format(page_number=str(page)))
+    soup = BeautifulSoup(r.content, "html.parser")
 
     content = soup.find("div", class_="m_listing-items_section")
     articles = content.find_all("article")
-    # print(len(articles))
-    for i in range(1, len(articles)):
-        # Get article text
+
+    if not articles:
+        break
+    else:
+        print(f"page number: {page}")
         for article in articles:
-            category = article.find(class_="b_categorical-heading")
-            # print(category.text)
-
-            title = article.find_all(class_="b_small-heading")[0]
-            date = article.find_all(class_="b_small-heading")[1]
-            print(title.text, date.text)
-
-            venue = article.find_all(class_="b_instructional-text")[0]
-            location = article.find_all(class_="b_instructional-text")[1]
-            # print(venue.text, location.text)
-
-            links = article.find("a")
-            # print(links.get("href"))
-
-            image = article.find("picture").find("img")
-            image_src = image.get("src")
-            image_alt = image.get("alt")
-            # print(image_src, image_alt)
+            print(getEventDetails(article))
+            print(getEventLink(article))
+            print(getEventImage(article))
