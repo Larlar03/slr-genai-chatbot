f288919698f04f27a564b6828e7e461fecf3a3aa
Lana Work <37781167+Larlar03@users.noreply.github.com>
2023-07-20 12:24:11 +0100

    Added to scraper to get event description

--- src/scraper.py
+++ src/scraper.py
@@ -5,16 +5,15 @@ from bs4 import BeautifulSoup
 import csv
 from dotenv import load_dotenv, find_dotenv
 
-# Get the directory path of the current script and find the .env file in it or its parent directories
-dotenv_path = os.path.join(os.path.dirname(__file__), "..", ".env")
-# Load the environment variables from the .env file
-_ = load_dotenv(dotenv_path)
-# Access the URL variable from the environment
+dotenv_path = os.path.join(
+    os.path.dirname(__file__), "..", ".env"
+)  # get dir path of current script and find the .env file in it or its parent directories
+_ = load_dotenv(dotenv_path)  # Load the environment variables from the .env file
 URL = os.environ.get("URL")
-
-# # to run cell # %%
+EVENT_URL = os.environ.get("EVENT_URL")
 
 page = requests.get(URL)
+events = []
 
 
 def getEventDetails(article):
@@ -43,10 +42,26 @@ def getEventImage(article):
         return image_src, image_alt
 
 
-events = []
+def getEventDescription(event_link):
+    event_page = requests.get(EVENT_URL.format(event_link=event_link))
+    soup = BeautifulSoup(event_page.content, "html.parser")
+
+    introduction = soup.find("p", class_="b_standfirst")
+    about = (
+        soup.find("section", class_="m_main-copy")
+        .find("div", class_="l_inner-grid")
+        .find("p")
+    )
+
+    if about:
+        description = introduction.text + "\n" + about.text
+    else:
+        description = introduction.text
+    return description
+
 
 for page in range(1, 20):
-    r = requests.get(URL.format(page_number=str(page), city="london"))
+    r = requests.get(URL.format(page_number=str(page), city="birmingham"))
     soup = BeautifulSoup(r.content, "html.parser")
     content = soup.find("div", class_="m_listing-items_section")
     articles = content.find_all("article")
@@ -60,12 +75,14 @@ for page in range(1, 20):
             details = getEventDetails(article)
             link = getEventLink(article)
             image = getEventImage(article)
+            description = getEventDescription(link)
             d["Category"] = details[0]
             d["Title"] = details[1]
             d["Date"] = details[2]
             d["Venue"] = details[3]
             d["Locations"] = details[4]
             d["Link"] = link
+            d["Description"] = description
             if image:
                 d["Image Src"] = image[0]
                 d["Image Alt"] = image[1]
@@ -84,6 +101,7 @@ with open(filename, "w", newline="") as f:
             "Link",
             "Image Src",
             "Image Alt",
+            "Description",
         ],
     )
     w.writeheader(),
