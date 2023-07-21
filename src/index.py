3e2eea2daed534e1b9c3ca9d77a73749c5e1d0b9
Lana Work <37781167+Larlar03@users.noreply.github.com>
2023-07-21 16:32:28 +0100

    Re-structured main script and tried web loader

--- src/index.py
+++ src/index.py
@@ -16,18 +16,14 @@ _ = load_dotenv(dotenv_path)
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 MONGODB_URI = os.environ.get("MONGODB_URI")
 
-# message_history = MongoDBChatMessageHistory(
-#     connection_string=MONGODB_URI, session_id="test-session"
-# )
-# message_history.add_user_message("hello world")
-# message_history.add_ai_message("whats up?")
-# print(message_history.messages)
-
-output_directory = "documents/faiss_db"
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.5)
 
-# build prompt
+message_history = MongoDBChatMessageHistory(
+    connection_string=MONGODB_URI, session_id="test-session"
+)
+
 prompt_template = """You are a help assistant at www.artrabbit.com having a conversation with a person who is looking for something creative adn cultural to do.
 Use the following pieces of context to provide a concise answer to the question.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -39,10 +35,10 @@ PROMPT = PromptTemplate.from_template(prompt_template)
 
 
 def load_vector_store():
-    # get OpenAi embeddings
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-    # load vector store
     db = os.path.join(os.getcwd(), "documents/faiss_db")
+
+    # load vector store
     vector_store = FAISS.load_local(db, embeddings)
     retriever = vector_store.as_retriever(search_kwargs={"k": 5})
 
@@ -59,11 +55,26 @@ def get_qa_chain():
     return qa_chain
 
 
+def store_chat(question, response):
+    message_history.add_user_message(question)
+    message_history.add_ai_message(response["answer"])
+
+
+def get_chat_history():
+    print(message_history.messages)
+
+
+def get_chat_messages():
+    ...
+
+
 def prompt_openai():
     qa_chain = get_qa_chain()
-    question = "I want to go to a photogrpahy exhibition next week. Whats on?"
+    question = "I want to go to an exhibition in July. Whats on?"
     response = qa_chain({"question": question})
+    store_chat(question=question, response=response)
     print(response["answer"])
+    print(message_history.messages)
 
 
 prompt_openai()
