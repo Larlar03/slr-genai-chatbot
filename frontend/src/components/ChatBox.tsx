bd9557cedb6a35467185c019f9dac7fa9e547d0b
Lana Work <37781167+Larlar03@users.noreply.github.com>
2023-07-27 17:41:00 +0100

    Editing styling

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -37,17 +37,16 @@ const ChatBox = ({ sessionId }: { sessionId: string }) => {
     };
 
     return (
-        <div id='ui-container' className='w-screen h-auto min-h-screen flex justify-center items-center'>
+        <div
+            id='ui-container'
+            className='bg-[url("assets/kristina-delp.jpg")] bg-cover w-screen h-auto min-h-screen flex justify-center items-center'
+        >
             <div className='p-4 bg-white shadow-lg rounded-lg sm:h-screen sm:w-screen lg:h-auto lg:w-4/6 xl:w-[682px] grid grid-cols-1 grid-rows-[1fr,min-content]'>
                 <div className='flex flex-col mb-4 space-y-6'>
                     <div className='chat bg-green'>
-                        <div className='flex items-center space-x-4 mb-5'>
-                            <img
-                                src='https://source.unsplash.com/600x600/?abstract'
-                                alt='User Avatar'
-                                className='w-10 h-10 rounded-full'
-                            />
-                            <h2 className='text-lg font-semibold'>Chatbot Name</h2>
+                        <div className='flex items-center space-x-4 mb-5 '>
+                            <div className='w-10 h-10 bg-gradient-to-r from-[#e0c3fc] to-[#8ec5fc] rounded-full mr-2' />
+                            <h2 className='text-lg font-semibold'>Single-Lens Reflex Chat</h2>
                         </div>
                         <div>
                             <div className='flex flex-col space-y-2'>
@@ -79,7 +78,7 @@ const ChatBox = ({ sessionId }: { sessionId: string }) => {
                         }}
                     />
                     <button
-                        className='px-4 py-2 ml-2 text-sm font-semibold text-white bg-blue-500 hover:bg-blue-600 rounded'
+                        className='px-4 py-2 ml-2 text-sm font-semibold text-white bg-[#3A59E4] hover:bg-[#667eea] rounded'
                         disabled={loading}
                         onClick={(event) => {
                             event.preventDefault();
